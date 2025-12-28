b3f58549d3c44346005183d7a538870cc118da93
May <halo_may@hotmail.com>
2025-12-28 15:49:45 +0000

    add new function

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
 ## this is my first script
 print("hello world")
 print("I am using github with posit.clound")
+
+def hello():
+  name = input("what is your name: ")
+  print(f"Hello {name}!")
+  
+  #test function
+  hello()
+  
+  
+  
